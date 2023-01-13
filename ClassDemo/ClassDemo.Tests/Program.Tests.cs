f48dc58cb24660f9629f414f8df771be5714e370
Evan Bartels <ebartels1@ewu.edu>
2023-01-12 18:46:32 -0800

    End of class Jan 12 2023

--- ClassDemo/ClassDemo.Tests/Program.Tests.cs
+++ ClassDemo/ClassDemo.Tests/Program.Tests.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+//this stuff very important to have
+
 namespace ClassDemo.Tests;
 
 [TestClass]
@@ -8,5 +11,34 @@ public class ProgramTests
     {
         string actual = Program.GetName("Inigo", "Montoya");
         Assert.AreEqual<string>("Inigo Montoya", actual);
+    }
+
+    string? Text = "Inigo Montoya";
+    // in your .tests file do
+    // <TreatWarningAsErrors>true</TreatWarningAsErrors> (might be enable, not true) <TreatWarningsAsErrors>true</TreatWarningsAsErrors>
+    // before turning in homework
+    void Method(string text)
+    {
+        if (Text is not null) 
+        { 
+            Text.Replace("n", "l");
+        }
+        string text1 = Text?.Replace("n", "l");
+
+        Text = text;
+    }
+
+    [TestMethod]
+    public void PlayingWithIntegers()
+    {
+        int number = 42;
+        Assert.AreEqual("Int32", number.GetType().Name);
+    }
+
+    [TestMethod]
+    public void PlayingWithStrings()
+    {
+        string number = "forty-two";
+        Assert.AreEqual("System.String", number.GetType().FullName);
     }
 }
